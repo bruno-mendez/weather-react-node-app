dd94bb9996d30354f62ca4b2f85bfb77842b96f6
Bruno Mendez <brunomendez@outlook.com>
2021-02-17 05:08:52 -0300

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,88 @@
+import React, { Component } from 'react';
+import { Alert, Col, Row, Spinner, Badge,Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
+
+import './App.css';
+
+class App extends Component {
+  state = {
+    response: '',
+    loading: '',
+    country: '',
+    responseToPost: '',
+    error:'',
+  };
+  
+  componentDidMount() {
+    this.setState({ loading: false });
+/*     this.callApi()
+      .then(res => this.setState({ response: res.express }))
+      .catch(err => console.log(err)); */
+  }
+  
+/*   callApi = async () => {
+    const response = await fetch('/api/weather');
+    const body = await response.json();
+    if (response.status !== 200) throw Error(body.message);
+    
+    return body;
+  }; */
+  
+  handleSubmit = async e => {
+    this.setState({ loading: true });
+    e.preventDefault();
+    const response = await fetch('api/weather', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ cityName: this.state.country }),
+    })
+    const body = await response.json();
+
+    this.setState({ responseToPost: body });
+    this.setState({ loading: false });
+  };
+  
+render() {
+    return (
+      <div className="App">
+         <h6><Badge color="light" pill>v1.0 | Dev by Bruno Mendez</Badge></h6>
+         <br></br>
+        <h1><Badge color="primary">How's The Weather In?</Badge></h1><br></br>
+        <Form action="api/weather"method ="post" onSubmit={this.handleSubmit}>
+            <FormGroup row>
+              <Input
+                type="text"
+                id="city"
+                value={this.state.country}
+                placeholder="City name"
+                onChange={e => this.setState({ country: e.target.value })}
+              />
+            </FormGroup>
+            <br></br>
+
+          <Button color="warning"type="submit">Search</Button>
+        </Form>
+        <br></br>
+
+        {this.state.responseToPost!=='' && !this.state.loading &&
+            <>
+                <img src={this.state.responseToPost.icon_url}></img>
+                <Alert color="primary"> Weather of {this.state.responseToPost.city}: {this.state.responseToPost.desc}</Alert> 
+                <Alert color="success"> Temperature: {this.state.responseToPost.temp} °C </Alert>
+                <Alert color="danger"> Max:  {this.state.responseToPost.max} °C </Alert>
+                <Alert color="secondary"> Min: {this.state.responseToPost.min} °C </Alert>
+                <Alert color="info"> Feels like: {this.state.responseToPost.feels} °C </Alert>
+            </>
+        }
+        {this.state.loading &&
+          <Spinner type="grow" color="primary" />
+        }
+       
+      </div>
+    );
+   
+  }
+}
+
+export default App;
---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const https = require('https');
+const bodyParser = require ('body-parser');
+
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: 'true'}));
+
+app.get("/", function(req, res){
+
+    res.sendFile(__dirname + "/index.html");
+    
+})
+
+app.post("/api/weather", function(req, res){
+    console.log(req.body);
+    const query = req.body.cityName;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${query}&APPID=2db14349cb3ec9d6324cca8280cb51cb&units=metric&lang=es`;
+    https.get(url, function (response){
+        console.log(response.data);
+
+        response.on("data", function(data){
+        const weatherData = JSON.parse(data);
+        const temp = weatherData.main.temp;
+        const description = weatherData.weather[0].description;
+        const icon = weatherData.weather[0].icon;
+        const min = weatherData.main.temp_min;
+        const max = weatherData.main.temp_max;
+        const feels = weatherData.main.feels_like;
+        const icon_url = `http://openweathermap.org/img/wn/${icon}@2x.png`;
+        console.log(temp);
+        console.log(description);
+/*         res.write(`<h1>El tiempo en ${query} actualmente es: ${description}</h1>`);
+        res.write("<h1>La temperatura es: " + temp + " celcius. </h1>");
+        res.write(`<img src=${icon_url} width="100">`); */
+        res.json({temp: temp, desc: description, city: query, icon_url: icon_url, max: max, min: min, feels: feels,
+            });
+        })
+    }); 
+})
+
+app.listen(8080, function (){
+    console.log("listening on port 8080");
+});
