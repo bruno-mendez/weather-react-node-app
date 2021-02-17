7da758306de1c23e157437075b30d37e8857ece7
Bruno Mendez <brunomendez@outlook.com>
2021-02-17 05:29:08 -0300

    server.js adjustment

--- server.js
+++ server.js
@@ -39,6 +39,16 @@ app.post("/api/weather", function(req, res){
     }); 
 })
 
+if (process.env.NODE_ENV === 'production') {
+    // Serve any static files
+    app.use(express.static(path.join(__dirname, 'client/build')));
+      
+    // Handle React routing, return all requests to React app
+    app.get('*', function(req, res) {
+      res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    });
+  }
+
 app.listen(8080, function (){
     console.log("listening on port 8080");
 });
